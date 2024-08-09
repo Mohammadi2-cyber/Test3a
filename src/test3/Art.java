8d8f9ab1e780d9896b8479b28312367409841bb7
Abdullah MOHAMMADI <ab.mohammadi791@gmail.com>
2024-08-08 20:16:01 -0400

    My new Test3

---
+++ src/test3/Art.java
@@ -0,0 +1,11 @@
+package test3;
+
+public class Art {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+        System.out.println("My name is Abdullah");
+        System.out.println("I'm going to learn Java");
+
+    }
+}
